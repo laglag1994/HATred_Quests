deb3c4081c62536f4e7bcf5f8dc8846f73fed02e
laglag1994 <roqayah1994@gmail.com>
2023-07-08 23:24:20 +0300

    auth

---
+++ src/pages/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,28 @@
+import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const handler = NextAuth({
+    providers: [
+        CredentialsProvider({
+            name: 'Credentials',
+            credentials: {
+                username: { label: "Username", type: "text", placeholder: "jsmith" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials, req) {
+                const res = await fetch("/your/endpoint", {
+                    method: 'POST',
+                    body: JSON.stringify(credentials),
+                    headers: { "Content-Type": "application/json" }
+                })
+                const user = await res.json()
+                if (res.ok && user) {
+                    return user
+                }
+                return null
+            }
+        })
+    ]
+})
+
+export { handler as GET, handler as POST }
